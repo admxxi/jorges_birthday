311a4b8ea0f6b514a1b195fb3e066cc5217070d5
Andre Monteiro <admxxi@gmail.com>
2019-02-15 15:50:16 +0000

    Index PHP + Composer.json fix

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andremonteiro
+ * Date: 2019-02-15
+ * Time: 15:45
+ */
+class Package {
+    public function __destruct()
+    {
+        echo "hi";
+    }
+}
